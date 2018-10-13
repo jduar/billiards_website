d35f0b7d5e115c7660d354db6aca444a097d1c85
Kamuish <33263706+Kamuish@users.noreply.github.com>
2018-10-13 17:27:48 +0100

    elo calculation

--- flaskproject/games/utils.py
+++ flaskproject/games/utils.py
@@ -15,3 +15,24 @@ def save_picture(form_picture):
 	i.save(picture_path)
 	return picture_fn
 
+
+def calculate_elo(p1_elo, p2_elo, winner, k_factor=20):
+
+	esp_score_p1 = 1 / (1 + 10 ** ((p2_elo - p1_elo) / 400))
+	esp_score_p2 = 1 / (1 + 10 ** ((p1_elo - p2_elo) / 400))
+
+	if winner is None:
+		score_p1 = score_p2 = 0.5
+
+	elif winner:
+		score_p1 = 1
+		score_p2 = 0
+
+	else:
+		score_p1 = 0
+		score_p2 = 1
+
+	new_elo_p1 = p1_elo + k_factor * (score_p1 - esp_score_p1)
+	new_elo_p2 = p2_elo + k_factor * (score_p2 - esp_score_p2)
+
+	return new_elo_p1, new_elo_p2
